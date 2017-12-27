cb2b976ecb22658d3e17eb14a42bac8c6904675a
panicbit <panicbit.dev@gmail.com>
2017-12-27 19:07:36 +0100

    Add required imports

--- src/bot.rs
+++ src/bot.rs
@@ -1,5 +1,7 @@
 use irc::client::prelude::{Config, IrcServer, Server, ServerExt, Command, ChannelExt};
 use failure::{Error, SyncFailure};
+use playground::{self, ExecuteRequest};
+use paste::paste;
 
 pub fn run() -> Result<(), Error> {
     let config = Config {
